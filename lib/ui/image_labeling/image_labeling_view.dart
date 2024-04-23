63a172a7fa0c6a3a038f596940ff50d0e808139e
Eren Elçi <elcierenn@gmail.com>
2024-04-23 17:26:20 +0300

    Image labeling bitti

--- lib/ui/image_labeling/image_labeling_view.dart
+++ lib/ui/image_labeling/image_labeling_view.dart
@@ -1,23 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:ml_kit_image_labeling_project/common/utils.dart';
+import 'package:ml_kit_image_labeling_project/ui/image_labeling/detector_view.dart';
 
-class ImageLabelingView extends StatelessWidget {
+class ImageLabelingView extends StatefulWidget {
   const ImageLabelingView({super.key});
 
   @override
+  State<ImageLabelingView> createState() => _ImageLabelingViewState();
+}
+
+class _ImageLabelingViewState extends State<ImageLabelingView> {
+  late ImageLabeler _imageLabeler;
+  bool _canProccess = false;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeLabeler();
+  }
+
+  @override
+  void dispose() {
+    _canProccess = false;
+    _imageLabeler.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(
-              "İmage Labeling",
-              style: TextStyle(fontSize: 30, color: Colors.black),
-            ),
-          )
-        ],
+      body: DetectorView(
+        title: "Image Labeler",
+        customPaint: _customPaint,
+        text: _text,
+        onImage: _processImage,
       ),
     );
+  }
+
+  Future<void> _initializeLabeler() async {
+    final path = 'assets/ml/object_labeler.tflite';
+    final modelPath = await getAssetPath(path);
+    final options = LocalLabelerOptions(modelPath: modelPath);
+    _imageLabeler = ImageLabeler(options: options);
+
+    _canProccess = true;
+  }
+
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProccess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    final labels = await _imageLabeler.processImage(inputImage);
+    if (inputImage.metadata?.size != null &&
+        inputImage.metadata?.rotation != null) {
+      final painter = LabelDetectorPainter(labels);
+      _customPaint = CustomPaint(painter: painter);
+    } else {
+      String text = 'Labels found: ${labels.length}\n\n';
+      for (final label in labels) {
+        text += 'Label: ${label.label}, '
+            'Confidence: ${label.confidence.toStringAsFixed(2)}\n\n';
+      }
+      _text = text;
+      _customPaint = null;
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
   }
 }
